9ffec5c9541288ad78661a87687c80649bce6d95
Mystic <manognachamarti@gmail.com>
2026-05-30 15:05:13 +0100

    Project init and setup - Endpoint, HandlerMethods, Mapping works

---
+++ src/main/java/com/manognachamarthi/taskticket/taskticketbackend/TaskticketbackendApplication.java
@@ -0,0 +1,13 @@
+package com.manognachamarthi.taskticket.taskticketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskticketbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskticketbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manognachamarthi/taskticket/taskticketbackend/TaskticketbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manognachamarthi.taskticket.taskticketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskticketbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
